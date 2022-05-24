7472252a051c6468f79e11fa628ffae5d2337ba6
David Rose <daviderose526@gmail.com>
2022-05-24 12:55:02 -0500

    initial commit

---
+++ src/test/java/com/example/reactpayroll/ReactPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
